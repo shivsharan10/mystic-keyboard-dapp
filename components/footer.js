fa89b79ba60dd60b7703ab8c1cb08aa1dd54fc51
Shivsharan Patil <101257452+shivsharan10@users.noreply.github.com>
2022-03-23 10:42:35 +0530

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
         target='_blank'
         rel='noopener noreferrer'
       >
-        Learn web3 dev and earn crypto rewards at{" "}
+        Developed using Solidity{" "}
         <span className=''>Pointer</span>
       </a>
-      <p>Art from Joanne Li @joanne on Figma <a href='https://keeybs.com' className='underline'>keeybs.com</a> <a href='https://creativecommons.org/licenses/by/4.0/' className="underline">CC 4.0</a></p>
+      <p>By Shivsharan Patil <a href='https://shivsharanpatil.github.io/shivsharanpatil/' className='underline'>My Portfolio</a> </p>
     </footer>
   )
 }
